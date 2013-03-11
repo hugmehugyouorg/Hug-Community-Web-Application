b72353064de6d2d0cbfaa00b075f87bd452cc14e
Andrew Welters <awelters@hugmehugyou.org>
2013-03-11 14:25:33 -0500

    initial commit

---
+++ application/controllers/welcome.php
@@ -0,0 +1,44 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->library('ion_auth');
+		$this->load->helper('url');
+	}
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		if (!$this->ion_auth->logged_in())
+		{
+			//redirect them to the login page
+			redirect('auth/login', 'refresh');
+		}
+		else if($this->ion_auth->is_admin()) {
+			//redirect them to the admin page
+			redirect('auth', 'refresh');
+		}
+		else
+			$this->load->view('welcome_message');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
