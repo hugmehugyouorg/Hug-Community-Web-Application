ee637e4b9e28b3cfe99078d26d6684f199e57330
Andrew Welters <awelters@olson.com>
2013-04-10 10:24:08 -0500

    Updated email templates based on Outlook.com marking as Junk

--- application/views/auth/email/activate.tpl.php
+++ application/views/auth/email/activate.tpl.php
@@ -1,6 +1,11 @@
 <html>
 <body>
-	<h1>Activate account for <?php echo $identity;?></h1>
+	<h1>Activate account</h1>
 	<p>Please click this link to <?php echo anchor('auth/activate/'. $id .'/'. $activation, 'Activate Your Account');?>.</p>
+<img src="http://www.hugmehugyou.org/img/logo.png"><p>The Hug Community Web Application is brought to you by the Hug Me Hug YOU! Initiative.</p>
+        <p>The Hug Me Hug YOU! Initiative is a Minnesota Non-Profit 317A Corporation with a mission to research, develop, manufacture, and freely distribute therapeutic companions to children in need in order to provide them with emotional support, to contribute to their overall sense of well-being, and to help each child stay connected to their support community. To rebuild job opportunities for low-income, disabled, and homeless individuals and create a strong sense of reciprocal altruism by providing meaningful work in the fields of design and technology.</p>
+        <p>Hug Me Hug YOU! Initiative<br>
+        2521 Fillmore St NE<br>
+        Minneapolis, MN 55418<br><a href="tel:612-396-7980">612-396-7980</a><br><a href="mailto:hug@hugmehugyou.org">hug@hugmehugyou.org</a><br><a href="http://www.hugmehugyou.org">http://www.hugmehugyou.org</a></p>
 </body>
 </html>
