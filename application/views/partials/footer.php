6d21bb6baad2906aa834d1949680bce61df6125b
Andrew Welters <awelters@olson.com>
2013-10-29 21:13:36 -0500

    styling footer changes for welcome screen

--- application/views/partials/footer.php
+++ application/views/partials/footer.php
@@ -2,14 +2,10 @@
 
 <div class="navbar navbar-fixed-bottom">
 	<div class="navbar-inner">
-	<div class="container">
-		<div class="row">
-		 	<div>
-		 	  <a href="http://www.hugmehugyou.org" target="_blank"><img class="hugmehugyou" src="//www.hugmehugyou.org/img/logo.png">
-			  <p>&copy;<?php echo(date("Y")); ?> Hug Me Hug YOU! Initiative</p></a>
-			</div>
+		<div class="container">
+			<a href="http://www.hugmehugyou.org" target="_blank"><img class="hugmehugyou" src="//www.hugmehugyou.org/img/logo.png">
+			<p>&copy;<?php echo(date("Y")); ?> Hug Me Hug YOU! Initiative</p></a>
 		</div>
-	</div>
 	</div>
 </div>
 
