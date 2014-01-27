cd4e237ee68142e9d5679b67451d7a846436e749
Andrew Welters <awelters@hugmehugyou.org>
2014-01-26 23:24:38 -0600

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -22,12 +22,6 @@
 		<p class="span4">
 			Abused children and children in foster care interact with a plush toy-like therapeutic companion that sends and receives messages. A support community or safety network made up of family, friends, and social workers interact with a web or mobile interface to invite and register community members, receive updates on the child's status, preview and send messages to the child, and view analytics to better understand and work with both the child and the community.
 		</p>
-		<p class="span4">
-			There will be at least 1 therapeutic companion for a Carver County Social Services initially for the Alpha and Beta Releases; in addition, we expect to deliver between 8 and 50 for Launch. We believe that for each therapeutic companion there will be approximately 5-10 members of a community receiving and sending messages to a child.
-		</p>
-		<p class="span4">
-			Finally, our plan is to partially or fully fund this project with Kickstarter and expect to offer similar therapeutic companions as toys in order to raise the amount necessary to make a production run within 6 months of the start of the Kickstarter project. We expect to have upwards of 500 additional therapeutic companions made if this is the case. This would mean there would be upwards of 5000 community members receiving and sending messages.	
-		</p>
 	</div>
 </div>
 
@@ -46,9 +40,15 @@
 </div>
 
 <div class="container-fluid">
-	<h3>In the Future...</h3>
+	<h3>The Plan</h3>
 	<div class="row-fluid">
-		<p class="span12">
+		<p class="span4">
+			There will be at least 1 therapeutic companion for a Carver County Social Services initially for the Alpha and Beta Releases; in addition, we expect to deliver between 8 and 50 for Launch. We believe that for each therapeutic companion there will be approximately 5-10 members of a community receiving and sending messages to a child.
+		</p>
+		<p class="span4">
+			Finally, our plan is to partially or fully fund this project with Kickstarter and expect to offer similar therapeutic companions as toys in order to raise the amount necessary to make a production run within 6 months of the start of the Kickstarter project. We expect to have upwards of 500 additional therapeutic companions made if this is the case. This would mean there would be upwards of 5000 community members receiving and sending messages.	
+		</p>
+		<p class="span4">
 			In the future, other children will be added from the same county, other children from other counties, and funds continued to be raised by using the donations for a toy approach. Our expectation is that within 2 years to have 2000 therapeutic companions in use and upwards of 20,000 community members receiving and sending messages.
 		</p>
 	</div>
