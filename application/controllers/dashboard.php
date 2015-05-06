70b12849910adee193cc564d1661e2c944500564
Andrew Welters <awelters@hugmehugyou.org>
2015-05-06 17:02:06 -0500

    Cache busting on polling

--- application/controllers/dashboard.php
+++ application/controllers/dashboard.php
@@ -322,6 +322,8 @@ class Dashboard extends MY_Controller {
 	}
 	
 	public function poll() {
+		$this->nocache();
+
 		if(!$this->input->is_ajax_request())
 		{
 			//redirect them to sign in
@@ -482,4 +484,12 @@ class Dashboard extends MY_Controller {
 
 		return '0 seconds';
 	}
+
+	protected function nocache() {
+        $this->output->set_header('Expires: Sat, 26 Jul 1997 05:00:00 GMT');
+        $this->output->set_header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+        $this->output->set_header('Cache-Control: no-cache, no-store, must-revalidate, max-age=0');
+        $this->output->set_header('Cache-Control: post-check=0, pre-check=0', FALSE);
+        $this->output->set_header('Pragma: no-cache');
+    }
 }
